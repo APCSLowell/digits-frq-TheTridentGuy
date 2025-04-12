173a02783a3c6ffeb75a84f0f8860613967d17d6
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-04-12 00:38:22 -0700

    pass yo damn tests

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,14 +6,28 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{
+		digitList = new ArrayList<Integer>();
+		if (num == 0) {
+			digitList.add(0);
+			return;
+		}
+		while(num >= 1) {
+			digitList.add(0, num % 10);
+			num = num / 10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	{
+		int last = digitList.get(0);
+		for (int i = 1; i < digitList.size(); i++) {
+			if (digitList.get(i) <= last) {
+				return false;
+			}
+			last = digitList.get(i);
+		}
+		return true;
 	}
 	
 	public String toString()
